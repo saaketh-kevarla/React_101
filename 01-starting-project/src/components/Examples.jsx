2d5180a6d174d43b8c39c252d207ea2838eac6b8
Saaketh Kevarla <saaketh.kevarla@erpa.com>
2025-05-28 16:17:46 -0400

    done

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { EXAMPLES } from "../data"
 import TabButton from "./TabButton"
 import { useState } from "react"
 import Section from "./Section"
+import Tabs from "./Tabs"
 
 export default function Examples() {
     const [A,setA] = useState()
@@ -12,12 +13,14 @@ export default function Examples() {
     return (
         <>
     <Section title = 'Examples' id = 'examples'>
-      <menu>
-        <TabButton onClick= {() => SelectHandler('components')} isSelected = {A === 'components'}>Components</TabButton>
+      {/* here we are passing the jsx as a prop to a component */}
+    <Tabs 
+    buttons = {<>
+      <TabButton onClick= {() => SelectHandler('components')} isSelected = {A === 'components'}>Components</TabButton>
         <TabButton onClick= {() => SelectHandler('jsx')} isSelected = {A === 'jsx'}>JSX</TabButton>
         <TabButton onClick= {() => SelectHandler('props')} isSelected = {A === 'props'}>Props</TabButton>
         <TabButton onClick= {() => SelectHandler('state')} isSelected = {A === 'state'}>State</TabButton>
-      </menu>
+      </>}>
       {!A ? <p>Please select a topic</p> : null}
       {A ? <div id='tab-content'>   
         <h3>{EXAMPLES[A].title}</h3>
@@ -28,6 +31,7 @@ export default function Examples() {
           </code>
         </pre>
       </div> : null}
+    </Tabs>
       </Section>
         </>
     )
