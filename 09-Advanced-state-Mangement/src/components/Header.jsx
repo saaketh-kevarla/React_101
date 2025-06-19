44227ea05ff85fd727aa6d73b8ef552bbc3e8e65
Saaketh Kevarla <saaketh.kevarla@erpa.com>
2025-06-19 17:41:55 -0400

    useContext React

--- 09-Advanced-state-Mangement/src/components/Header.jsx
+++ 09-Advanced-state-Mangement/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { useRef } from 'react';
+import { useRef,useContext } from 'react';
 
 import CartModal from './CartModal.jsx';
+import { CartContext } from '../store/shopping-cart-context.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header() {
   const modal = useRef();
+  const {items} = useContext(CartContext)
 
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
@@ -22,12 +24,12 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
     );
   }
 
+  
+
   return (
     <>
       <CartModal
         ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
         title="Your Cart"
         actions={modalActions}
       />
