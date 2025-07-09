ab715a18a229205ddc4b9ff7d2507b2d9d4c6f71
Saaketh Kevarla <saaketh.kevarla@erpa.com>
2025-07-09 16:09:12 -0400

    end of useState section

--- 10-sideEffects-useEffect/src/components/DeleteConfirmation.jsx
+++ 10-sideEffects-useEffect/src/components/DeleteConfirmation.jsx
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ProgressBar from "./ProgressBar";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  
+
   useEffect(() => {
     console.log('TIMER SET');
     const timer = setTimeout(() => {
       onConfirm();
-    },3000)
+    },TIMER)
 
     return () =>{
       console.log('Cleaning up Timer');
       clearTimeout(timer);
     }
-  },[])
+  },[onConfirm])
   
-
-
-
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -29,6 +29,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar />
     </div>
   );
 }
